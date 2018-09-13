47b8c2e1311528517dbc29e75cc75bb35e160cc4
llj <979520102@qq.com>
2018-09-13 14:58:15 +0800

    test github for vs code

--- src/category/CategoryScreen.js
+++ src/category/CategoryScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
     View,
     Text,
+    StyleSheet
 } from 'react-native';
 
 export default class CategoryScreen extends Component {
@@ -16,3 +17,10 @@ export default class CategoryScreen extends Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        justifyContent: 'flex-start'
+    }
+});
