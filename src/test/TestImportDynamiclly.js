ffe846304e69a49b64865d95cafeddbc48f915d6
llj <979520102@qq.com>
2018-10-17 18:14:08 +0800

    Clare
    test import

---
+++ src/test/TestImportDynamiclly.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+
+export default class TestImportDynamiclly extends Component {
+    render() {
+        return (
+            <View style={{backgroundColor: 'cyan'}}>
+                <Text>Test Import Dynamiclly</Text>
+            </View>
+        );
+    }
+}
